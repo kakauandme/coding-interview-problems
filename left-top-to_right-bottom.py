f9444644259cac466641c751be29280831879128
Kirill Kliavin <k.kliavin@gmail.com>
2016-06-08 14:23:56 +1000

    left-top-to_right-bottom
    Print all paths from left top corner to bottom right corner of a
    matrix. You can move right or down in each step.

---
+++ left-top-to_right-bottom.py
@@ -0,0 +1,37 @@
+'''
+Print all paths from left top corner to bottom right corner of a matrix. You can move right or down in each step. 
+
+Example:
+[['a', 'b', 'c'], 
+['d', 'e', 'f']] 
+
+Output: 
+a->b->c->f 
+a->b->e->f 
+a->d->e->f
+'''
+    
+
+a = [['a', 'b', 'c'], 
+	 ['d', 'e', 'f']]
+
+n = len(a)
+m = len(a[0])
+
+
+def nextStep(i,j,output):
+
+	output+=str(a[i][j])
+	if i+1 == n and j+1 == m:
+		print(output)
+		return
+
+	else:
+		output+="->"
+		if i+1 < n:
+			nextStep(i+1,j,output)
+
+		if j+1 < m:
+			nextStep(i,j+1,output)
+
+nextStep(0,0,"")
